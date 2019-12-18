c5847584807db989d9b41c8e3f0585287c2172b5
Ronnen Nagal <nagalr@gmail.com>
2019-12-18 09:29:20 -0500

    Creates Users.js module and the middleware directory

--- server/index.js
+++ server/index.js
@@ -3,15 +3,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
-const PORT = 3001;
+const users = require('../Users');
+const PORT = 3000;
 
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send('This is the homepage.');
+    res.send(users[0].name);
 });
 
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 });
